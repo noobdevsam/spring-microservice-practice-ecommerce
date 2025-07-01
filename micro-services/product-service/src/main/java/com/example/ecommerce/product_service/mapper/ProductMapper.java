595cb5b2ade269cc369e167af467f3dc04849cba
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-01 14:49:45 +0600

    add ProductMapper for converting between Product entities and DTOs

---
+++ micro-services/product-service/src/main/java/com/example/ecommerce/product_service/mapper/ProductMapper.java
@@ -0,0 +1,54 @@
+package com.example.ecommerce.product_service.mapper;
+
+import com.example.ecommerce.product_service.entities.Category;
+import com.example.ecommerce.product_service.entities.Product;
+import com.example.ecommerce.product_service.model.ProductPurchaseResponse;
+import com.example.ecommerce.product_service.model.ProductRequestDTO;
+import com.example.ecommerce.product_service.model.ProductResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public Product productRequestDTOtoProduct(ProductRequestDTO productRequestDTO) {
+        if (productRequestDTO == null) {
+            return null;
+        }
+
+        return Product.builder()
+                .id(productRequestDTO.id())
+                .name(productRequestDTO.name())
+                .description(productRequestDTO.description())
+                .price(productRequestDTO.price())
+                .availableQuantity(productRequestDTO.availableQuantity())
+                .category(
+                        Category.builder()
+                                .id(productRequestDTO.categoryId())
+                                .build()
+                )
+                .build();
+    }
+
+    public ProductResponseDTO productToProductResponseDTO(Product product) {
+        return new ProductResponseDTO(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getAvailableQuantity(),
+                product.getPrice(),
+                product.getCategory().getId(),
+                product.getCategory().getName(),
+                product.getCategory().getDescription()
+        );
+    }
+
+    public ProductPurchaseResponse productToProductPurchaseResponse(Product product, double quantity) {
+        return new ProductPurchaseResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                quantity
+        );
+    }
+}
