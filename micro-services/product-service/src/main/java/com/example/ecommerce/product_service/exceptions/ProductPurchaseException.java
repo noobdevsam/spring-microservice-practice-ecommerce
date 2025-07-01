51c6ad74335c0f499b5d4c4cd33df9595f0e1be7
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-01 15:14:53 +0600

    add ProductPurchaseException for handling product purchase errors

---
+++ micro-services/product-service/src/main/java/com/example/ecommerce/product_service/exceptions/ProductPurchaseException.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce.product_service.exceptions;
+
+public class ProductPurchaseException extends RuntimeException {
+
+    public ProductPurchaseException(String message) {
+        super(message);
+    }
+
+}
