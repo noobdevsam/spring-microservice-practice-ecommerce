ee1a13d1db595049039ac39997e1f1845ae178d0
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-02 21:33:07 +0600

    add auditing support to CustomerOrder entity #21

--- micro-services/order-service/src/main/java/com/example/ecommerce/order_service/entities/CustomerOrder.java
+++ micro-services/order-service/src/main/java/com/example/ecommerce/order_service/entities/CustomerOrder.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class CustomerOrder {
 
     @Id
