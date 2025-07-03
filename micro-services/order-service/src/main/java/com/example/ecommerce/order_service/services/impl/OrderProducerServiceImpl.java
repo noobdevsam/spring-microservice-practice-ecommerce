82211c512c531b0b3e6f1eaccb56827eb0243512
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-03 20:09:42 +0600

    add order confirmation message sending logic using Kafka

--- micro-services/order-service/src/main/java/com/example/ecommerce/order_service/services/impl/OrderProducerServiceImpl.java
+++ micro-services/order-service/src/main/java/com/example/ecommerce/order_service/services/impl/OrderProducerServiceImpl.java
@@ -4,6 +4,10 @@ import com.example.ecommerce.order_service.models.OrderConfirmationDTO;
 import com.example.ecommerce.order_service.services.OrderProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +15,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderProducerServiceImpl implements OrderProducerService {
 
+    private final KafkaTemplate<String, OrderConfirmationDTO> kafkaTemplate;
+
     @Override
     public void sendOrderConfirmation(OrderConfirmationDTO orderConfirmationDTO) {
-        // Here you would implement the logic to send the order confirmation message
-        // For example, you might use a message broker like Kafka or RabbitMQ
-        // For now, we will just log the order confirmation
-        log.info("Sending order confirmation: {}", orderConfirmationDTO);
+        log.info("Sending order confirmation.....");
+
+        Message<OrderConfirmationDTO> message = MessageBuilder
+                .withPayload(orderConfirmationDTO)
+                .setHeader(KafkaHeaders.TOPIC, "order-topic")
+                .build();
+
+        kafkaTemplate.send(message);
     }
 }
