a9afc7d0169e55e5c65b59d084d103fe9b41e11a
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-05 19:47:54 +0600

    add PaymentServiceImpl for payment processing and notification handling

---
+++ micro-services/payment-service/src/main/java/com/example/ecommerce/payment_service/services/impl/PaymentServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.ecommerce.payment_service.services.impl;
+
+import com.example.ecommerce.payment_service.mappers.PaymentMapper;
+import com.example.ecommerce.payment_service.models.PaymentNotificationRequestDTO;
+import com.example.ecommerce.payment_service.models.PaymentRequestDTO;
+import com.example.ecommerce.payment_service.repositories.PaymentRepository;
+import com.example.ecommerce.payment_service.services.NotificationProducerService;
+import com.example.ecommerce.payment_service.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+    private final NotificationProducerService notificationProducerService;
+
+    @Override
+    public Integer createPayment(PaymentRequestDTO paymentRequestDTO) {
+
+        var payment = paymentRepository.save(
+                paymentMapper.paymentRequestDTOToPayment(paymentRequestDTO)
+        );
+
+        notificationProducerService.sendNotification(
+                new PaymentNotificationRequestDTO(
+                        paymentRequestDTO.orderReference(),
+                        paymentRequestDTO.amount(),
+                        paymentRequestDTO.paymentMethod(),
+                        paymentRequestDTO.customerDTO().firstName(),
+                        paymentRequestDTO.customerDTO().lastName(),
+                        paymentRequestDTO.customerDTO().email()
+                )
+        );
+
+        return paymentRequestDTO.id();
+    }
+}
