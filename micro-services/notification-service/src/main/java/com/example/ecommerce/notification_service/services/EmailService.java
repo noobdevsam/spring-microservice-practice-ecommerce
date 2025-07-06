3221df9ece7bb4dd29bc01bd6dc4fb99a34792b8
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-06 13:59:08 +0600

    add exception handling for email sending in EmailService

--- micro-services/notification-service/src/main/java/com/example/ecommerce/notification_service/services/EmailService.java
+++ micro-services/notification-service/src/main/java/com/example/ecommerce/notification_service/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce.notification_service.services;
 
 import com.example.ecommerce.notification_service.models.ProductDTO;
+import jakarta.mail.MessagingException;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,7 +13,7 @@ public interface EmailService {
             String customerName,
             BigDecimal amount,
             String orderReference
-    );
+    ) throws MessagingException;
 
     void sendOrderConfirmationEmail(
             String destinationEmail,
@@ -20,5 +21,5 @@ public interface EmailService {
             BigDecimal amount,
             String orderReference,
             List<ProductDTO> productDTOs
-    );
+    ) throws MessagingException;
 }
