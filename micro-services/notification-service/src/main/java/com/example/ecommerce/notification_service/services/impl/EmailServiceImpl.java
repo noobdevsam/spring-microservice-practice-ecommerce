7c95cc59fc4b3b7a10dd87aad6b47ac74c2905aa
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-06 14:24:50 +0600

    add email sending logic with template processing for payment successful in EmailServiceImpl

--- micro-services/notification-service/src/main/java/com/example/ecommerce/notification_service/services/impl/EmailServiceImpl.java
+++ micro-services/notification-service/src/main/java/com/example/ecommerce/notification_service/services/impl/EmailServiceImpl.java
@@ -2,15 +2,23 @@ package com.example.ecommerce.notification_service.services.impl;
 
 import com.example.ecommerce.notification_service.models.ProductDTO;
 import com.example.ecommerce.notification_service.services.EmailService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.example.ecommerce.notification_service.models.EmailTemplates.PAYMENT_CONFIRMATION;
 
 @Service
 @Slf4j
@@ -27,7 +35,35 @@ public class EmailServiceImpl implements EmailService {
             String customerName,
             BigDecimal amount,
             String orderReference
-    ) {
+    ) throws MessagingException {
+
+        var mimeMessage = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(
+                mimeMessage,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name()
+        );
+
+        final var templateName = PAYMENT_CONFIRMATION.getTemplate();
+        messageHelper.setSubject(PAYMENT_CONFIRMATION.getSubject());
+        messageHelper.setFrom("moodxmail@gmail.com");
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("amount", amount);
+        variables.put("orderReference", orderReference);
+
+        var context = new Context();
+        context.setVariables(variables);
+
+        try {
+            var htmlTemplate = templateEngine.process(templateName, context);
+            messageHelper.setTo(destinationEmail);
+            messageHelper.setText(htmlTemplate, true);
+            log.info("INFO - Sending mail to email: {} with template {}", destinationEmail, templateName);
+        } catch (MessagingException _) {
+            log.warn("WARNING - Error while sending email to {} :", destinationEmail);
+        }
 
     }
 
@@ -39,7 +75,7 @@ public class EmailServiceImpl implements EmailService {
             BigDecimal amount,
             String orderReference,
             List<ProductDTO> productDTOs
-    ) {
+    ) throws MessagingException {
 
     }
 
