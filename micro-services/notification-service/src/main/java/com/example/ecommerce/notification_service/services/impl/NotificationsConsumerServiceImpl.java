472f6c7c61f6e5dfaff335bf257fa9c7113d64f3
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-06 14:54:54 +0600

    add payment success notification handling in NotificationsConsumerServiceImpl

--- micro-services/notification-service/src/main/java/com/example/ecommerce/notification_service/services/impl/NotificationsConsumerServiceImpl.java
+++ micro-services/notification-service/src/main/java/com/example/ecommerce/notification_service/services/impl/NotificationsConsumerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce.notification_service.services.impl;
 
+import com.example.ecommerce.notification_service.entities.Notification;
+import com.example.ecommerce.notification_service.models.NotificationType;
 import com.example.ecommerce.notification_service.models.OrderConfirmationDTO;
 import com.example.ecommerce.notification_service.models.PaymentConfirmationDTO;
 import com.example.ecommerce.notification_service.repositories.NotificationRepository;
@@ -11,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -21,7 +25,28 @@ public class NotificationsConsumerServiceImpl implements NotificationsConsumerSe
 
     @KafkaListener(topics = "payment-topic")
     @Override
-    public void consumePaymentSuccessNotifications(PaymentConfirmationDTO paymentConfirmationDTO) throws MessagingException {
+    public void consumePaymentSuccessNotifications(
+            PaymentConfirmationDTO paymentConfirmationDTO
+    ) throws MessagingException {
+
+        log.info("Consuming the message from payment-topic Topic :: {}", paymentConfirmationDTO);
+
+        notificationRepository.save(
+                Notification.builder()
+                        .notificationType(NotificationType.PAYMENT_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .paymentConfirmationDTO(paymentConfirmationDTO)
+                        .build()
+        );
+
+        var customerName = paymentConfirmationDTO.customerFirstName() + " " + paymentConfirmationDTO.customerLastName();
+
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmationDTO.customerEmail(),
+                customerName,
+                paymentConfirmationDTO.amount(),
+                paymentConfirmationDTO.orderReference()
+        );
 
     }
 
