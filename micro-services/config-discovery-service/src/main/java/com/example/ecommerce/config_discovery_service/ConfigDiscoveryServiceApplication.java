d98ee9f10c16396ea63afe3a78fbf5812cb7ce31
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-07 23:17:00 +0600

    add initial configuration for config discovery service

---
+++ micro-services/config-discovery-service/src/main/java/com/example/ecommerce/config_discovery_service/ConfigDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.config_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigDiscoveryServiceApplication.class, args);
+    }
+
+}
