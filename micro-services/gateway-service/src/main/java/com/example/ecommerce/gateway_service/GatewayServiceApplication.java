9cb570fab91307d484b4408192f1c39e712d9d47
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-08 23:39:37 +0600

    add WebFlux security configuration with OAuth2 resource server support

--- micro-services/gateway-service/src/main/java/com/example/ecommerce/gateway_service/GatewayServiceApplication.java
+++ micro-services/gateway-service/src/main/java/com/example/ecommerce/gateway_service/GatewayServiceApplication.java
@@ -2,9 +2,32 @@ package com.example.ecommerce.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@EnableWebFluxSecurity
 @SpringBootApplication
 public class GatewayServiceApplication {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http
+    ) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(
+                        authorize ->
+                                authorize.pathMatchers("/eureka/**").permitAll()
+                                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(Customizer.withDefaults())
+                )
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
