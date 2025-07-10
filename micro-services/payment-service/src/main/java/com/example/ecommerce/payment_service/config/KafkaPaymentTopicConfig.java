43edfc8ef33521148bd5c1db1c877e4cef3208b1
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-10 16:48:39 +0600

    add Javadoc comments to KafkaPaymentTopicConfig for improved documentation

--- micro-services/payment-service/src/main/java/com/example/ecommerce/payment_service/config/KafkaPaymentTopicConfig.java
+++ micro-services/payment-service/src/main/java/com/example/ecommerce/payment_service/config/KafkaPaymentTopicConfig.java
@@ -5,9 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+/**
+ * Configuration class for Kafka topics related to the Payment Service.
+ * Defines the topics used for payment notifications.
+ */
 @Configuration
 public class KafkaPaymentTopicConfig {
 
+    /**
+     * Creates a Kafka topic named "payment-topic".
+     * This topic is used for sending payment notifications.
+     *
+     * @return a {@link NewTopic} object representing the "payment-topic".
+     */
     @Bean
     public NewTopic paymentNotificationTopic() {
         return TopicBuilder
