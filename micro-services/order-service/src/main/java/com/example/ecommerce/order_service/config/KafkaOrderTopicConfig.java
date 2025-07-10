e3b6bf9cca1c34ef93defc09fd24e449ba443020
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-10 14:13:41 +0600

    Revert "add Javadoc comments to KafkaOrderTopicConfig for improved configuration documentation"
    This reverts commit 94315cfed1ddb1223334519427ddebedb2f758be.

--- micro-services/order-service/src/main/java/com/example/ecommerce/order_service/config/KafkaOrderTopicConfig.java
+++ micro-services/order-service/src/main/java/com/example/ecommerce/order_service/config/KafkaOrderTopicConfig.java
@@ -5,22 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
-/**
- * Configuration class for Kafka topics related to the order service.
- * Defines beans for creating Kafka topics.
- */
 @Configuration
 public class KafkaOrderTopicConfig {
 
-    /**
-     * Creates a new Kafka topic named "order-topic".
-     *
-     * @return A NewTopic object representing the "order-topic".
-     */
     @Bean
     public NewTopic orderTopic() {
         return TopicBuilder
-                .name("order-topic") // Sets the name of the topic
-                .build(); // Builds the topic configuration
+                .name("order-topic")
+                .build();
     }
 }
