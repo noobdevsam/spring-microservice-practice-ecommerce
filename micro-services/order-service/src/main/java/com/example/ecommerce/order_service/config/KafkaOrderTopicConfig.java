17e36954b8d31c2bbb41984711ff80f22e2d8b32
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-10 14:19:09 +0600

    add Javadoc comments to KafkaOrderTopicConfig for improved configuration documentation

--- micro-services/order-service/src/main/java/com/example/ecommerce/order_service/config/KafkaOrderTopicConfig.java
+++ micro-services/order-service/src/main/java/com/example/ecommerce/order_service/config/KafkaOrderTopicConfig.java
@@ -5,13 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+/**
+ * Configuration class for defining Kafka topics related to the order service.
+ * This class provides beans for creating and configuring Kafka topics.
+ */
 @Configuration
 public class KafkaOrderTopicConfig {
 
+    /**
+     * Creates a Kafka topic named "order-topic".
+     * This topic is used for handling order-related events in the system.
+     *
+     * @return A NewTopic object representing the "order-topic".
+     */
     @Bean
     public NewTopic orderTopic() {
         return TopicBuilder
-                .name("order-topic")
-                .build();
+                .name("order-topic") // Specifies the name of the Kafka topic.
+                .build(); // Builds and returns the topic configuration.
     }
 }
